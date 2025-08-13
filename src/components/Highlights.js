f35f65889b3d15b4bb3f473ac30363b8da1f3947
ilie_ioana_laura@yahoo.com <ilie_ioana_laura@yahoo.com>
2025-08-13 13:46:46 +0300

    started Higylights-fixed some css for sections

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -1,10 +1,44 @@
 import React from 'react'
-
+import gsap from 'gsap'
+import '../styles/Highlights.css'
+import { useGSAP } from '@gsap/react'
+import { watchImg } from '../helpers';
+import {rightImg} from '../helpers';
 function Highlights() {
+ useGSAP(() => {
+  gsap.to('#title', {
+    opacity: 1,
+    y: 0,
+    duration: 0.5,
+    delay: 3,
+    stagger: 0.2
+  });
+  gsap.to('.link', {
+    opacity: 1,
+    y: 0,
+    duration: 1,
+    delay: 3,
+    stagger: 0.25
+  });
+}, []);
+
   return (
-    <div>
-      
-    </div>
+    <section id='highlights' >
+    <div className='highlights-content'>
+      <div className='margin'>
+      <h1 id='title' className='section-heading'> Get the Highlights</h1>
+      <div className='extra'>
+        <p className='link'>Watch the film
+          <img src={watchImg} alt="Watch" width={20} height={20} style={{ marginLeft: '5px' }} />
+        </p>
+        <p className='link'>Watch the event
+          <img src={rightImg} alt="Right Arrow" width={20} height={20} style={{ marginLeft: '5px' }} />
+        </p>
+
+      </div>
+      </div>
+      </div>
+    </section>
   )
 }
 
