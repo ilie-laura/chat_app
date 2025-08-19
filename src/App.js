793368d7211bc36f2a3d1cca8616ef6526fe9619
laura <162310131+ilie-laura@users.noreply.github.com>
2025-08-19 13:37:48 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 import "./styles/App.css";
+import Features from "./components/Features";
 const App = () => {
   return (
    <main className="App">
    <Navbar />
    <Hero />
    <Highlights />
+   <Model />
+   <Features />
+   <HowItWorks />
+
    </main>
   );
 }
